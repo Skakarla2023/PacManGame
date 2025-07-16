5e2aada1b9f396cf5118a7d86a7f58a999d2cd56
Satwika Kakarla <skakarla2023@gmail.com>
2025-07-16 21:21:22 +0530

    Initial commit of PacMan game

---
+++ src/App.java
@@ -0,0 +1,32 @@
+import javax.swing.JFrame;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int rowCount=21;
+        int columnCOunt=19;
+        int tileSize=32;
+        int boardWidth=columnCOunt*tileSize;
+        int boardHeight=rowCount*tileSize;
+
+        JFrame frame = new JFrame("Pac Man");
+        //frame.setVisible(true);
+        frame.setSize(boardWidth,boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        // to close the game if the player clicks on the X on the top of the window
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Jpanel to draw on and show our game
+
+
+        PacMan pacmanGame=new PacMan();
+        frame.add(pacmanGame);
+        frame.pack();
+        pacmanGame.requestFocus();
+        frame.setVisible(true);
+
+
+
+
+
+    }
+}
